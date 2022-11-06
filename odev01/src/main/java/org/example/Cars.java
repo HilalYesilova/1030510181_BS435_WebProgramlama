4b33ead32865aa0fa7d302073871bc28b146b609
Hilal Yeşilova <hilal.yesilova0311@gmail.com>
2022-11-06 16:05:21 +0300

    odev01 denemesi

---
+++ odev01/src/main/java/org/example/Cars.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+}
